0859710f2fc4e06c01f2325537dbd4297b29034b
mintusarker <sarkermintu707@gmail.com>
2023-04-06 23:42:51 +0600

    cart

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { HiArrowLeft } from "react-icons/hi2";
+import './Cart.css'
+
 
 const Cart = () => {
     const cart = useSelector(state => state.cart)
     console.log(cart);
 
     return (
-        <div>
+        <div className='cart-container'>
             <h2>Shopping Cart</h2>
             {
                 cart.cartItems.length === 0 ? (
@@ -20,13 +23,13 @@ const Cart = () => {
                         </div>
                     </div>
                 ) : (<div>
-                    <div className='title'>
-                        <h3>Product</h3>
-                        <h3>Price</h3>
-                        <h3>Quantity</h3>
-                        <h3>Total</h3>
+                    <div className='titles'>
+                        <h3 className='product-title'>Product</h3>
+                        <h3 className='price'>Price</h3>
+                        <h3 className='quantity'>Quantity</h3>
+                        <h3 className='total'>Total</h3>
                     </div>
-                    <div className='cart-item'>
+                    <div className='cart-items'>
                         {
                             cart.cartItems.map(cartItem => (
                                 <div className="cart-item" key={cartItem._id}>
@@ -48,6 +51,23 @@ const Cart = () => {
                                 </div>
                             ))
                         }
+
+                        <div className="cart-summary">
+                            <button className='clear-btn'>Clear Cart</button>
+                            <div className='checkout'>
+                                <div className="sub-total">
+                                    <spam>Subtotal</spam>
+                                    <span className='amount'>${cart.cartTotalAmount}</span>
+                                </div>
+                                <p>Tax and shipping calculated at checkout</p>
+                                <button>Check Out</button>
+                                <Link to='/'><div className='shopping'>
+                                    <HiArrowLeft/>
+                                    <span>Continue shopping</span>
+                                </div></Link>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
                 )
